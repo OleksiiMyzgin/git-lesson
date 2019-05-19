831545be554283c316b711662efd9b317864b164
OleksiiMyzgin <krulatuy@gmail.com>
2019-05-19 08:30:41 +0300

    add intro and footer section

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>Hello, my name is Alex</h1>
+      <h2>I am a frontend developer</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h2>This is a git lesson</h2>
       </header>
+      <footer>
+      Copyright 2019 Alexey Myzgin
+      </footer>
     </div>
   );
 }
