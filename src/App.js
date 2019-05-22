6111633409bb4aac326b2bc9e2a43e61b9ee3297
Oleksii Myzgin <oleksii.myzgin@qvantel.com>
2019-05-22 09:31:48 +0300

    add app-refactoring:
    commit 38e821152b9eee9943dba75e1c2ca1e88c91af9a
    Author: Oleksii Myzgin <oleksii.myzgin@qvantel.com>
    Date:   Tue May 21 10:07:30 2019 +0300
    
        add app footer
    
    commit d13b462ea304c95a23fb8a23d9107c398d04344f
    Author: Oleksii Myzgin <oleksii.myzgin@qvantel.com>
    Date:   Tue May 21 09:34:57 2019 +0300
    
        update README
    
    commit 74708e65040e05e485f79883bc1cae7f152b5492
    Author: Oleksii Myzgin <oleksii.myzgin@qvantel.com>
    Date:   Thu May 16 12:07:28 2019 +0300
    
        add title

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Hello, my name is Alex</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h2>This is a git lesson</h2>
       </header>
+      <div class="footer">
+  App footer
+</div>
     </div>
   );
 }
